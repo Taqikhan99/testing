d5e4aefd773ba036af4c699d6131597c5ddacbcb
taqi khan <taqiahmed339@gmail.com>
2020-09-21 10:22:08 +0500

    Add project files.

---
+++ testing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            Console.WriteLine("x is: " + x);
+        }
+    }
+}
